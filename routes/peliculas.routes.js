3ca00dbee53de9bebd07ccc3a1f2aaffcf7ecd2c
daniel_romero_martinez_unique <daniel.romero@bluestart.mx>
2024-05-03 23:25:54 -0700

    Practica node auth

---
+++ routes/peliculas.routes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+const peliculas = require('../controllers/peliculas.controller')
+
+router.get('/', peliculas.getAll);
+
+router.get('/:id', peliculas.get);
+
+router.post('/', peliculas.create);
+
+router.put('/:id', peliculas.update);
+
+router.delete('/:id', peliculas.delete);
+
+module.exports = router
